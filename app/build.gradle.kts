4b9a826c75c881479177b1b11ed37b37a3161c14
Yashesh Shah <38342167+dev-Blaze@users.noreply.github.com>
2026-07-01 15:30:33 -0700

    Bump version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.yshah.alfred"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildFeatures {
